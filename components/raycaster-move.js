5183546983d5e552af86ac99bf0ae54811055956
Michael <mch.9975@gmail.com>
2020-05-29 17:35:48 +0900

    Fixed Rotate Camera Trigger Move

--- components/raycaster-move.js
+++ components/raycaster-move.js
@@ -1,3 +1,6 @@
+
+var curr_raycast_pos = new THREE.Vector3();
+var drag_filter_offset = 0.01;
 AFRAME.registerComponent('raycaster-listen', {
     init: function () {
       // Use events to figure out what raycaster is listening so we don't have to
@@ -5,15 +8,22 @@ AFRAME.registerComponent('raycaster-listen', {
       this.el.addEventListener('raycaster-intersected', evt => {
         this.raycaster = evt.detail.el;
       });
-      
+
       this.el.addEventListener('raycaster-intersected-cleared', evt => {
         this.raycaster = null;
       });
 
-      this.el.addEventListener('click', () => {
+      this.el.addEventListener('mousedown', () => {
+        if (!this.raycaster) { return; }  // Not intersecting.
+        curr_raycast_pos = this.raycaster.components.raycaster.getIntersection(this.el).point;
+      });
+
+      this.el.addEventListener('mouseup', () => {
         if (!this.raycaster) { return; }  // Not intersecting.
         let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
         if (!intersection) { return; } // Null checker
+
+        if(!curr_raycast_pos || curr_raycast_pos.distanceTo(intersection.point) > drag_filter_offset) { return; }
 
         var camera = document.querySelector("#camera"); // Get camera by id
         var cam_curr_pos = camera.getAttribute('position'); // Get camera current position
